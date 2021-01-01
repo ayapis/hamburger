e2af3ee71f0664b18ab5d6d37cbf5c70ff53a9bd
ayapis <ayapisss@gmail.com>
2021-01-02 02:43:02 +0900

    jQueryの微調整

--- hamburger.js
+++ hamburger.js
@@ -5,21 +5,22 @@
 //   } );
 // } );
 
-
-$(window).on('load resize', function(){
-  var w = $(window).width();
-  var x = 768;
-  if (w <= x) {
-//画面サイズが768px以下のときの処理
-    $( ".c-button-open" ).on( "click", function() {
-      $( ".l-aside" ).addClass( "is-open" );
-      $( ".c-button-close" ).addClass( "is-open" );
-      $( ".l-aside__title" ).css( 'display' , 'none' );
-    } );
-    $( ".c-button-close" ).on( "click", function() {
-      $( ".l-aside" ).removeClass( "is-open" );
-      $( ".c-button-close" ).removeClass( "is-open" );
-      $( ".l-aside__title" ).css( 'display' , '' );
-    } );
-  }
-});
+jQuery( function( $ ){
+  $(window).on('load resize', function(){
+    var w = $(window).width();
+    var x = 768;
+    if (w <= x) {
+      //画面サイズが768px以下のときの処理
+      $( ".c-button-open" ).on( "click", function() {
+        $( ".l-aside" ).addClass( "is-open" );
+        $( ".c-button-close" ).addClass( "is-open" );
+        $( ".l-aside__title" ).css( 'display' , 'none' );
+      } );
+      $( ".c-button-close" ).on( "click", function() {
+        $( ".l-aside" ).removeClass( "is-open" );
+        $( ".c-button-close" ).removeClass( "is-open" );
+        $( ".l-aside__title" ).css( 'display' , '' );
+      } );
+    }
+  });
+} );
